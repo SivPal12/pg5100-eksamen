c518aaff61ba2787175a822a8a41a5bba851fe6e
Pal M Sivertsen <palmarius@hotmail.com>
2014-11-12 14:31:51 +0100

    Modified db

--- src/main/java/no/nith/sivpal12/pg5100/eksamen/pojos/Artist.java
+++ src/main/java/no/nith/sivpal12/pg5100/eksamen/pojos/Artist.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Artist {
@@ -13,7 +13,7 @@ public class Artist {
     @GeneratedValue(strategy = GenerationType.TABLE)
     private int id;
 
-    @OneToOne
+    @ManyToOne
     private Genre genre;
     private String name;
 
