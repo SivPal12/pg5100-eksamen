815ee9d37c6a8f35077c130f261e0c45831dc247
Pal M Sivertsen <palmarius@hotmail.com>
2014-11-15 13:36:08 +0100

    Added some more dummy data

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -3,3 +3,11 @@ INSERT INTO Genre VALUES (2,'blues');
 INSERT INTO Genre VALUES (3,'country');
 INSERT INTO Genre VALUES (4,'hard');
 INSERT INTO Genre VALUES (5,'rock');
+
+INSERT INTO Artist VALUES (1,'Buckcherry',5);
+INSERT INTO Artist VALUES (2,'Johnny Cash',3);
+INSERT INTO Artist VALUES (3,'Seether',5);
+INSERT INTO Artist VALUES (4,'Meghan Trainor',1);
+INSERT INTO Artist VALUES (5,'The Blues Brothers',2);
+INSERT INTO Artist VALUES (5,'AC/DC',4);
+INSERT INTO Artist VALUES (5,'Kiss',2);
