56a8ae70fd8d9465e7260d2152cd81dad929c644
Pal M Sivertsen <palmarius@hotmail.com>
2014-11-16 21:56:58 +0100

    Ensured unique artist name at creation

--- src/main/java/no/nith/sivpal12/pg5100/eksamen/controllers/ArtistController.java
+++ src/main/java/no/nith/sivpal12/pg5100/eksamen/controllers/ArtistController.java
@@ -2,12 +2,18 @@ package no.nith.sivpal12.pg5100.eksamen.controllers;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
+import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
+import javax.inject.Named;
+import javax.validation.constraints.Pattern;
 
+import no.nith.sivpal12.pg5100.eksamen.constraints.UniqueArtistName;
 import no.nith.sivpal12.pg5100.eksamen.dao.ArtistDao;
 import no.nith.sivpal12.pg5100.eksamen.pojos.Artist;
 
+import org.hibernate.validator.constraints.NotEmpty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,23 +25,37 @@ public class ArtistController {
     @Inject
     private ArtistDao artistDao;
 
-    private Artist artist = new Artist();
+    @Named
+    @Produces
+    private Artist artist;
+
+    @UniqueArtistName
+    @NotEmpty
+    @Pattern(regexp = ".*\\S+.*",
+            message = "{org.hibernate.validator.constraints.NotEmpty.message}")
+    private String uniqueArtistName;
 
     public List<Artist> getAllArtists() {
         return artistDao.allArtists();
     }
 
     public void save() {
+        artist.setName(uniqueArtistName);
         LOGGER.trace(String.format("Saving %s", artist));
         artistDao.save(artist);
+        initArtist();
+    }
+
+    public String getUniqueArtistName() {
+        return uniqueArtistName;
+    }
+
+    public void setUniqueArtistName(String uniqueArtistName) {
+        this.uniqueArtistName = uniqueArtistName;
+    }
+
+    @PostConstruct
+    private void initArtist() {
         artist = new Artist();
-    }
-
-    public Artist getArtist() {
-        return artist;
-    }
-
-    public void setArtist(Artist artist) {
-        this.artist = artist;
     }
 }
