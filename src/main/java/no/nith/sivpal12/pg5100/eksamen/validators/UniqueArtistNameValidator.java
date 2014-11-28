46c219cc0805c0f58b22e451ca6979f6e98980f9
Pal M Sivertsen <palmarius@hotmail.com>
2014-11-28 16:13:15 +0100

    Added test for unique concert name

--- src/main/java/no/nith/sivpal12/pg5100/eksamen/validators/UniqueArtistNameValidator.java
+++ src/main/java/no/nith/sivpal12/pg5100/eksamen/validators/UniqueArtistNameValidator.java
@@ -9,9 +9,14 @@ import javax.validation.ConstraintValidatorContext;
 import no.nith.sivpal12.pg5100.eksamen.constraints.UniqueArtistName;
 import no.nith.sivpal12.pg5100.eksamen.pojos.Artist;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Stateless
 public class UniqueArtistNameValidator implements
         ConstraintValidator<UniqueArtistName, String> {
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(UniqueConcertNameValidator.class);
 
     @PersistenceContext(unitName = "myManager")
     private EntityManager entityManager;
@@ -22,9 +27,16 @@ public class UniqueArtistNameValidator implements
 
     @Override
     public boolean isValid(String artistName, ConstraintValidatorContext context) {
-        return entityManager
+        boolean isValid = entityManager
                 .createNamedQuery(Artist.NAMED_QUERY_BY_NAME, Artist.class)
                 .setParameter(1, artistName)
                 .getResultList().isEmpty();
+
+        if (!isValid) {
+            LOGGER.debug(String.format("Artist name '%s' is not unique!",
+                    artistName));
+        }
+
+        return isValid;
     }
 }
